aec372b4574a29c9ddee2e9fb8cef44f0cb6ffb8
Owain Jones <tehdoomcat@gmail.com>
2010-06-23 13:17:32 +0100

    added .Motion parsing class

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import net
+import net,motion
 
 class Nao:
 	def __init__(self,joystick,id=0,team='blue',socket=None):
@@ -15,6 +15,7 @@ class Nao:
 		self.id = id
 		self.team = team
 		self.pos = teams[team][id]
+		self.m = motion.Motion('bots/nao/joints.txt','bots/nao/Forwards.motion')
 
 	def think(self):
 		if self.init == 0:
@@ -25,14 +26,20 @@ class Nao:
 				self.s.send('(init unum '+str(self.id)+')(TeamName '+self.team+'))')
 				self.init = 2
 		elif self.init == 2:
-			if self.state == 0 and self.idle == False: self.s.send('(he1 0)(lae1 0)')
-			elif self.state == 5: self.s.send('(he1 '+str(0.1+self.speed)+')')
-			elif self.state == 6: self.s.send('(he1 '+str(-0.1-self.speed)+')')
-			elif self.state == 7: self.s.send('(lae1 '+str(0.1+self.speed)+')')
-			elif self.state == 8: self.s.send('(lae1 '+str(-0.1-self.speed)+')')
-		if self.state in range(1,8):
-			self.idle = False
-		print self.init, self.state
+#			if self.state == 0 and self.idle == False: self.s.send('(he1 0)(lae1 0)')
+#			elif self.state == 5: self.s.send('(he1 '+str(0.1+self.speed)+')')
+#			elif self.state == 6: self.s.send('(he1 '+str(-0.1-self.speed)+')')
+#			elif self.state == 7: self.s.send('(lae1 '+str(0.1+self.speed)+')')
+#			elif self.state == 8: self.s.send('(lae1 '+str(-0.1-self.speed)+')')
+#		if self.state in range(1,8):
+#			self.idle = False
+#		print self.init, self.state
+			frame = self.m.next()
+			buffer = ''
+			for joint in frame:
+				buffer += '('+joint+' '+str(frame[joint])+')'
+			self.s.send(buffer)
+			
 
 	def idle(self):
 		self.state = 0
---
+++ motion.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python -OO
+# Parser class - read SimSpark .motion files into a format that RCSS can understand.
+# Takes .motion files and 'limit.txt's, which define the maximum/minimum amount each
+# of a robot's limbs can move in Webots & RCSS, and the names of the joints in both.
+
+class Motion:
+	def __init__(self,limits=None,motion=None):
+		self.rcssjoints = []
+		if limits:
+			self.set_limits(limits)
+		if motion:
+			self.set_motions(motion)
+		self.index = -1
+
+	def set_limits(self,file):
+		tmpfile = open(file)
+		limits = {}
+		for line in tmpfile:
+			if line[0] == chr(35): continue #if line starts with a hash, it's a comment.
+			tmp = line.split("\t\t")
+			if tmp[1] not in ['',"\n",' ',"\r","\r\n"]: self.rcssjoints.append(tmp[1][:-1])
+			tmp = tmp[0].split(',')
+			limits[tmp[0]] = (round(float(tmp[1]),2),round(float(tmp[2][0:-1]),2))
+		self.limits = limits
+
+	def set_limit(self,joint,tuple):
+		try: self.limits[joint]
+		except KeyError:
+			print 'No such joint'
+			return
+		else:
+			self.limits[joint] = tuple
+
+	def set_motions(self,file):
+		tmpfile = open(file)
+		motions = {}
+		for line in tmpfile:
+			tmp = line.split(',')
+			if line[0] == chr(35): #lines starting with hashes define joints used in motion.
+				for element in tmp:
+					if element in self.limits:
+						motions[element] = []
+			else:
+				keys = motions.keys()
+				for i,element in enumerate(tmp):
+					if i>=2: #first two variables seem to be junk
+						motions[keys[i-3]].append(tmp[i])
+		self.motions = motions
+
+	def get_frame(self,frame=0):
+		tmp = {}
+		for i,key in enumerate(self.motions):
+			try:
+				tmp[self.rcssjoints[i]] = round(float(self.motions[key][frame]),1)
+			except IndexError:
+				return {}
+		return tmp
+
+	def next(self):
+		if self.index < self.get_length():
+			self.index += 1
+		else:
+			self.index = 0
+		return self.get_frame(self.index)
+
+	def prev(self):
+		if self.index > 0:
+			self.index -= 1
+		else:
+			self.index = 0
+		return self.get_frame(self.index)
+
+	def get_length(self):
+		return len(self.motions[self.motions.keys()[0]])
+
+if __name__=='__main__':
+	nao = Motion('bots/nao/joints.txt','bots/nao/Forwards.motion')
+#	nao.set_limit('LAnkleRoll',(0,-13.37))
+	for i in range(100):
+		frame = nao.next()
+		print frame
